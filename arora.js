289b79cea64dafc1cc28a9d3e7fd38231997a31b
Ilya-Marvin <ilya44.m@gmail.com>
2021-04-14 15:19:23 -0400

    test

---
+++ arora.js
@@ -0,0 +1,18 @@
+// $(()=>{
+    
+//     $('.fas.fa-bars').click(function(){
+//     $('.nav-menu li').toggle("slow",
+//         function(){$('.nav-menu li').css("display","flex");
+// }
+
+    
+        
+//     )
+// })
+
+
+// })
+
+
+
+
